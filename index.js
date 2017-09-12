0fd30e14638a1631ad03dfc791768d302df8b85c
Aidan Wolk <aidanwolk@comcast.net>
2017-09-11 20:00:29 -0700

    Create project and package description

---
+++ index.js
@@ -0,0 +1,17 @@
+var interceptor = require('express-interceptor');
+
+exports = interceptor(function (req, res) {
+    return {
+        isInterceptable: function () {
+            return /text\/html/.test(res.get('Content-Type'));
+        },
+        intercept: function (body, send) {
+            var regex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/gi;
+            send(body.replace(regex, function (match) {
+                return Array.prototype.map.call(match, function (chr) {
+                    return '&#' + chr.charCodeAt(0) + ';';
+                }).join('');
+            }));
+        }
+    }
+});
